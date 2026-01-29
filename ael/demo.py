d6fda558db79cdffb36e41c1caf0207f23487f49
Michael Fatemi <myfatemi04@gmail.com>
2026-01-29 15:44:54 -0500

    further improve the demo visualization

---
+++ ael/demo.py
@@ -0,0 +1,145 @@
+"""A CLI that demonstrates solving a problem from a problem set and saving the results."""
+
+import time
+from dataclasses import dataclass, field
+from pathlib import Path
+
+import numpy as np
+from loguru import logger
+
+from ael.problem import Problem
+from ael.solve import DEFAULT_SCHEDULE, OptimizerOptions, Result, ScheduleEntry, solve
+from ael.visualize import save_optimization_process_video, save_video, visualize
+
+PROBLEM_SET_ROOT_DIR = Path(__file__).resolve().parents[1] / "instances_data"
+
+
+@dataclass
+class MainArgs:
+    problem_set: str = "dense"
+    """ Loads problems from `./instances_data/instances_{problem_set}.json`. """
+
+    sample_index: int | None = None
+    """ If specified, only solves the problem at this index in the problem set. If not specified, selects the first problem. """
+
+    num_robots: int | None = None
+    """ If specified, selects the first problem with this many robots. Cannot be simultaneously specified with `sample_index`. """
+
+    optimizer: OptimizerOptions = field(default_factory=OptimizerOptions)
+    """ Options for the optimizer to use. """
+
+    schedule: str = "default"
+    """ Path to a JSON file specifying the schedule to use, or 'default'. """
+
+    save_dir: str = "./results/demo_{date}T{time}"
+    """ Path to a directory in which to save results. Allows formatting with `date` and `time` variables, which are formatted as YYYY-mm-dd and HH-MM-SS, respectively. """
+
+
+def store_result(problem: Problem, result: Result, save_dir: Path):
+    """Helper function that stores all tensors. Also stores high-level information like the time taken to solve, maximum constraint violations, and objective function values. Furthermore, visualizes the optimization process and final trajectories."""
+
+    import json
+
+    import matplotlib.pyplot as plt
+
+    # Save tensors.
+    np.savez_compressed(
+        save_dir / "result.npz",
+        trajectories=np.array(result.trajectories),
+        agent_obstacle_constraint_residuals=result.constraint_satisfaction.agent_obstacle_constraint_residuals,
+        agent_agent_constraint_residuals=result.constraint_satisfaction.agent_agent_constraint_residuals,
+        velocity_constraint_residuals=result.constraint_satisfaction.velocity_constraint_residuals,
+    )
+
+    # Save high-level information.
+    info_dict = {
+        "solve_time": result.solve_time,
+        "identifier": result.identifier,
+        "max_constraint_residuals": {
+            "agent_obstacle": float(
+                np.max(
+                    result.constraint_satisfaction.agent_obstacle_constraint_residuals
+                )
+            ),
+            "agent_agent": float(
+                np.max(result.constraint_satisfaction.agent_agent_constraint_residuals)
+            ),
+            "velocity": float(
+                np.max(result.constraint_satisfaction.velocity_constraint_residuals)
+            ),
+        },
+    }
+    with open(save_dir / "info.json", "w") as f:
+        json.dump(info_dict, f, indent=4)
+
+    # Visualize final trajectories.
+    visualize(problem, plt.gca(), result.trajectories[-1])
+    plt.clf()
+    # Visualize optimization process.
+    save_optimization_process_video(
+        problem, result.trajectories, save_dir / "optimization_process.mp4"
+    )
+    # Visualize final trajectory.
+    save_video(problem, result.trajectories[-1], save_dir / "final_trajectory.mp4")
+
+
+def main(args: MainArgs):
+    import json
+
+    problem_set_path = PROBLEM_SET_ROOT_DIR / f"instances_{args.problem_set}.json"
+    if not problem_set_path.exists():
+        raise ValueError(f"Problem set path {problem_set_path} does not exist.")
+
+    with open(problem_set_path) as f:
+        problems = json.load(f)
+
+    if args.sample_index is not None and args.num_robots is not None:
+        raise ValueError("Cannot simultaneously specify sample_index and num_robots.")
+
+    if args.sample_index is not None:
+        if args.sample_index < 0 or args.sample_index >= len(problems):
+            raise ValueError("sample_index out of range.")
+
+        problem = Problem.from_json(problems[args.sample_index], type="numpy")
+
+    elif args.num_robots is not None:
+        problem = None
+        for pd in problems:
+            if pd["num_agents"] == args.num_robots:
+                problem = Problem.from_json(pd, type="numpy")
+                break
+        else:
+            raise ValueError(f"No problem found with {args.num_robots} robots.")
+
+    else:
+        problem = Problem.from_json(problems[0], type="numpy")
+
+    if args.schedule == "default":
+        schedule = DEFAULT_SCHEDULE
+    else:
+        schedule_path = Path(args.schedule)
+        if not schedule_path.exists():
+            raise ValueError(f"Schedule path {schedule_path} does not exist.")
+
+        with open(schedule_path) as f:
+            schedule_json = json.load(f)
+
+        schedule = [ScheduleEntry(**entry_dict) for entry_dict in schedule_json]
+
+    result = solve(problem=problem, optimizer_options=args.optimizer, schedule=schedule)
+    logger.info(f"Solved problem in {result.solve_time:.2f} seconds.")
+
+    save_dir = Path(
+        args.save_dir.format(
+            date=time.strftime("%Y-%m-%d"), time=time.strftime("%H-%M-%S")
+        )
+    )
+    save_dir.mkdir(parents=True, exist_ok=True)
+    store_result(problem, result, save_dir)
+
+
+if __name__ == "__main__":
+    import tyro
+
+    args = tyro.cli(MainArgs)
+    main(args)
--- ael/solve.py
+++ ael/solve.py
@@ -1,18 +1,27 @@
 """
-The main solving algorithm. If used with the CLI, allows running a demo script.
+The main solving algorithm.
 """
 
 import time
-from dataclasses import dataclass, field
-from pathlib import Path
+from dataclasses import dataclass
 from typing import Literal
 
 import numpy as np
-from loguru import logger
 
 from ael.problem import Problem
 from ael.score_function import compute_score
-from ael.visualize import save_optimization_process_video, save_video
+
+
+@dataclass
+class ConstraintSatisfaction:
+    agent_obstacle_constraint_residuals: np.ndarray
+    """ Residuals for agent-obstacle nonpenetration constraints. If positive, represents the amount by which the signed distance into the obstacle exceeds zero. Shape (t, num_agents, num_obstacles). """
+
+    agent_agent_constraint_residuals: np.ndarray
+    """ Residuals for agent-agent nonpenetration constraints. If positive, represents the amount by which the signed distance into another agent exceeds zero. Shape (t, num_agents, num_agents). """
+
+    velocity_constraint_residuals: np.ndarray
+    """ Residuals for velocity constraints. If positive, represents the amount by which the velocity exceeds the maximum allowed velocity. Shape (t-1, num_agents). """
 
 
 @dataclass
@@ -26,11 +35,7 @@ class Result:
     identifier: str | None
     """ An identifier for the problem that was solved, if available. """
 
-    agent_obstacle_constraint_residuals: np.ndarray
-    """ Residuals for agent-obstacle nonpenetration constraints. If positive, represents the amount by which the signed distance into the obstacle exceeds zero. Shape (t, num_agents, num_obstacles). """
-
-    agent_agent_constraint_residuals: np.ndarray
-    """ Residuals for agent-agent nonpenetration constraints. If positive, represents the amount by which the signed distance into another agent exceeds zero. Shape (t, num_agents, num_agents). """
+    constraint_satisfaction: ConstraintSatisfaction
 
 
 @dataclass
@@ -72,13 +77,14 @@ class ScheduleEntry:
 DEFAULT_SCHEDULE = [
     ScheduleEntry(sigma=0.01, step_size=0.5, num_steps=60, kinetic_weight=50),
     ScheduleEntry(sigma=0.001, step_size=0.5, num_steps=60, kinetic_weight=10),
-    ScheduleEntry(sigma=0.0001, step_size=0.5, num_steps=60, kinetic_weight=2),
+    ScheduleEntry(sigma=0.001, step_size=0.5, num_steps=30, kinetic_weight=2),
+    ScheduleEntry(sigma=0.001, step_size=0.5, num_steps=30, kinetic_weight=0.01),
 ]
 
 
 def compute_constraint_residuals(
     problem: Problem[np.ndarray], trajectories: np.ndarray
-) -> tuple[np.ndarray, np.ndarray]:
+) -> ConstraintSatisfaction:
     """Return the constraint residuals for agent-obstacle and agent-agent distances, respectively."""
 
     ### Agent-agent nonpenetration constraints ###
@@ -90,11 +96,13 @@ def compute_constraint_residuals(
     min_acceptable_distances = (
         problem.agent_radii[:, np.newaxis] + problem.agent_radii[np.newaxis, :]
     )
-    residuals_agent_agent = min_acceptable_distances - agent_pairwise_distances
-    residuals_agent_agent[residuals_agent_agent < 0] = 0.0
-    diag_indices = np.diag_indices(residuals_agent_agent.shape[1])
-    residuals_agent_agent[:, diag_indices[0], diag_indices[1]] = 0.0
-    assert (np.diagonal(residuals_agent_agent, axis1=1, axis2=2) == 0).all()
+    agent_agent_constraint_residuals = (
+        min_acceptable_distances - agent_pairwise_distances
+    )
+    agent_agent_constraint_residuals[agent_agent_constraint_residuals < 0] = 0.0
+    diag_indices = np.diag_indices(agent_agent_constraint_residuals.shape[1])
+    agent_agent_constraint_residuals[:, diag_indices[0], diag_indices[1]] = 0.0
+    assert (np.diagonal(agent_agent_constraint_residuals, axis1=1, axis2=2) == 0).all()
 
     ### Agent-obstacle nonpenetration constraints ###
     agent_obstacle_distances = (
@@ -106,12 +114,22 @@ def compute_constraint_residuals(
         # (num_agents, 1) + (1, num_obstacles) -> (num_agents, num_obstacles)
         problem.agent_radii[:, np.newaxis] + problem.obstacle_radii[np.newaxis, :]
     )
-    residuals_agent_obstacle = (
+    agent_obstacle_constraint_residuals = (
         min_acceptable_agent_obstacle_distances - agent_obstacle_distances
     )
-    residuals_agent_obstacle[residuals_agent_obstacle < 0] = 0.0
+    agent_obstacle_constraint_residuals[agent_obstacle_constraint_residuals < 0] = 0.0
 
-    return residuals_agent_obstacle, residuals_agent_agent
+    ### Velocity constraints ###
+    velocities = trajectories[1:] - trajectories[:-1]
+    speeds = np.linalg.norm(velocities, axis=-1)
+    velocity_constraint_residuals = speeds - problem.agent_max_speeds[np.newaxis, :]
+    velocity_constraint_residuals[velocity_constraint_residuals < 0] = 0.0
+
+    return ConstraintSatisfaction(
+        agent_obstacle_constraint_residuals,
+        agent_agent_constraint_residuals,
+        velocity_constraint_residuals,
+    )
 
 
 def solve(
@@ -181,135 +199,11 @@ def solve(
 
     solve_time = time.time() - t0
 
-    agent_obstacle_constraint_residuals, agent_agent_constraint_residuals = (
-        compute_constraint_residuals(problem, trajectory)
-    )
+    constraint_satisfaction = compute_constraint_residuals(problem, trajectory)
 
     return Result(
         solve_time=solve_time,
         trajectories=trajectories,
         identifier=identifier,
-        agent_obstacle_constraint_residuals=agent_obstacle_constraint_residuals,
-        agent_agent_constraint_residuals=agent_agent_constraint_residuals,
+        constraint_satisfaction=constraint_satisfaction,
     )
-
-
-@dataclass
-class MainArgs:
-    problem_set: str = "dense"
-    """ Loads problems from `./instances_data/instances_{problem_set}.json`. """
-
-    sample_index: int | None = None
-    """ If specified, only solves the problem at this index in the problem set. If not specified, selects the first problem. """
-
-    num_robots: int | None = None
-    """ If specified, selects the first problem with this many robots. Cannot be simultaneously specified with `sample_index`. """
-
-    optimizer: OptimizerOptions = field(default_factory=OptimizerOptions)
-    """ Options for the optimizer to use. """
-
-    schedule: str = "default"
-    """ Path to a JSON file specifying the schedule to use, or 'default'. """
-
-    save_dir: str = "./results/demo_{date}T{time}"
-    """ Path to a directory in which to save results. Allows formatting with `date` and `time` variables, which are formatted as YYYY-mm-dd and HH-MM-SS, respectively. """
-
-
-PROBLEM_SET_ROOT_DIR = Path(__file__).resolve().parents[1] / "instances_data"
-
-
-def store_result(problem: Problem, result: Result, save_dir: Path):
-    """Helper function that stores all tensors. Also stores high-level information like the time taken to solve, maximum constraint violations, and objective function values. Furthermore, visualizes the optimization process and final trajectories."""
-
-    import json
-
-    # Save tensors.
-    np.savez_compressed(
-        save_dir / "result.npz",
-        trajectories=np.array(result.trajectories),
-        agent_obstacle_constraint_residuals=result.agent_obstacle_constraint_residuals,
-        agent_agent_constraint_residuals=result.agent_agent_constraint_residuals,
-    )
-
-    # Save high-level information.
-    info_dict = {
-        "solve_time": result.solve_time,
-        "identifier": result.identifier,
-        "max_agent_obstacle_constraint_residual": float(
-            np.max(result.agent_obstacle_constraint_residuals)
-        ),
-        "max_agent_agent_constraint_residual": float(
-            np.max(result.agent_agent_constraint_residuals)
-        ),
-    }
-    with open(save_dir / "info.json", "w") as f:
-        json.dump(info_dict, f, indent=4)
-
-    # Visualize optimization process.
-    save_optimization_process_video(
-        problem, result.trajectories, save_dir / "optimization_process.mp4"
-    )
-    # Visualize final trajectory.
-    save_video(problem, result.trajectories[-1], save_dir / "final_trajectory.mp4")
-
-
-def main(args: MainArgs):
-    import json
-
-    problem_set_path = PROBLEM_SET_ROOT_DIR / f"instances_{args.problem_set}.json"
-    if not problem_set_path.exists():
-        raise ValueError(f"Problem set path {problem_set_path} does not exist.")
-
-    with open(problem_set_path) as f:
-        problems = json.load(f)
-
-    if args.sample_index is not None and args.num_robots is not None:
-        raise ValueError("Cannot simultaneously specify sample_index and num_robots.")
-
-    if args.sample_index is not None:
-        if args.sample_index < 0 or args.sample_index >= len(problems):
-            raise ValueError("sample_index out of range.")
-
-        problem = Problem.from_json(problems[args.sample_index], type="numpy")
-
-    elif args.num_robots is not None:
-        problem = None
-        for pd in problems:
-            if pd["num_agents"] == args.num_robots:
-                problem = Problem.from_json(pd, type="numpy")
-                break
-        else:
-            raise ValueError(f"No problem found with {args.num_robots} robots.")
-
-    else:
-        problem = Problem.from_json(problems[0], type="numpy")
-
-    if args.schedule == "default":
-        schedule = DEFAULT_SCHEDULE
-    else:
-        schedule_path = Path(args.schedule)
-        if not schedule_path.exists():
-            raise ValueError(f"Schedule path {schedule_path} does not exist.")
-
-        with open(schedule_path) as f:
-            schedule_json = json.load(f)
-
-        schedule = [ScheduleEntry(**entry_dict) for entry_dict in schedule_json]
-
-    result = solve(problem=problem, optimizer_options=args.optimizer, schedule=schedule)
-    logger.info(f"Solved problem in {result.solve_time:.2f} seconds.")
-
-    save_dir = Path(
-        args.save_dir.format(
-            date=time.strftime("%Y-%m-%d"), time=time.strftime("%H-%M-%S")
-        )
-    )
-    save_dir.mkdir(parents=True, exist_ok=True)
-    store_result(problem, result, save_dir)
-
-
-if __name__ == "__main__":
-    import tyro
-
-    args = tyro.cli(MainArgs)
-    main(args)
